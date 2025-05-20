7aa75c337de0e9463cb1a16370a35d6c3a1698a0
Sohcahtoaii <98654303+Sohcahtoaii@users.noreply.github.com>
2025-05-20 17:26:16 +0200

    Update KAH_EMTT_EQ43.ino

--- Emetteur/KAH_EMTT_EQ43.ino
+++ Emetteur/KAH_EMTT_EQ43.ino
@@ -16,7 +16,7 @@
 #define BoutonPoussoir_Pin           9        // Pin numérique 8 pour le bouton poussoir 
 #define LEDInfrarouge_Pin            11        // Pin numérique 9 pour la LED infrarouge (à modifier selon la configuration du projet)
 #define LEDverte                      A5       // Pin numérique 9 pour la LED infrarouge
-#define NumeroEquipe                 0x43    // Identifiant unique de l'équipe (0x52 en hexadécimal, à personnaliser)
+// #define NumeroEquipe                 0x43    // Identifiant unique de l'équipe (0x52 en hexadécimal, à personnaliser)
 
 // Définition des fonctions d'acquisition
 // Fonction pour obtenir la valeur du potentiomètre de la vitesse
@@ -44,24 +44,24 @@ uint8_t AcquerirBoutonPoussoir() {
 
 // Définition des fonctions de traitement
 // Fonction pour calculer les données NEC à partir de la vitesse et de la direction
-uint8_t CalculerDonneeNEC(uint16_t Vitesse, uint16_t Direction) { 
-  static int Donnee[8] = {0};  // Tableau pour stocker les 8 bits de données
-  int drctin = (Vitesse << 5) | (Direction & 0x1F);  // Combine la vitesse et la direction dans une seule valeur (3 bits pour la vitesse et 5 bits pour la direction)
-  for (int i = 7; i >= 0; i--) {
-    Donnee[i] = ((drctin >> i) & 1);  // Remplie le tableau Donnee avec les bits de la donnée calculée
-  }
-  return Donnee;  // Retourne les 8 bits sous forme de tableau (probablement à convertir en un octet plus tard)
+uint8_t CalculerDonneeNEC(uint16_t Vitesse, uint16_t Direction) {
+  Vitesse = Vitesse & 0x07;     // 3 bits
+  Direction = Direction & 0x1F; // 5 bits
+
+  uint8_t drctin = (Vitesse << 5) | Direction;
+  return drctin;
 }
 
+
 // Fonction pour calculer l'adresse NEC à partir de l'état du Klaxon et du numéro de l'équipe
-uint8_t CalculerAdresseNEC(uint8_t Klaxon) { 
-  static int Adresse[8] = {0};  // Tableau pour stocker les 8 bits de l'adresse
-  int adrss = (Klaxon << 7) | (NumeroEquipe & 0x7F);  // Combine l'état du Klaxon (1 bit) avec le numéro de l'équipe (7 bits)
-  for (int i = 7; i >= 0; i--) {
-    Adresse[i] = ((adrss >> i) & 1);  // Remplie le tableau Adresse avec les bits de l'adresse calculée
-  }
-  return Adresse;  // Retourne l'adresse (probablement à convertir en un octet plus tard)
+uint8_t CalculerAdresseNEC(uint8_t Klaxon) {
+  uint8_t NumeroEquipe = 0x43;  // 67 en décimal
+  Klaxon &= 0x01;               // Assure que Klaxon est bien sur 1 bit
+  NumeroEquipe &= 0x7F;         // Assure que Numéro équipe est bien sur 7 bits
+  return (Klaxon << 7) | NumeroEquipe;
 }
+
+
 
 // Définition des fonctions principales
 // Fonction d'initialisation du système
